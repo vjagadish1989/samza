60e05b2cdad4093db91c4d9d71fe0d20080ccea3
Boris Shkolik <boryas@apache.org>
2016-09-22 11:53:05 -0700

    SAMZA-1019 Reopen a Kafka system producer if send() method is called again

--- samza-kafka/src/main/scala/org/apache/samza/system/kafka/KafkaSystemProducer.scala
+++ samza-kafka/src/main/scala/org/apache/samza/system/kafka/KafkaSystemProducer.scala
@@ -20,8 +20,7 @@
 package org.apache.samza.system.kafka
 
 
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Future
+import java.util.concurrent.{TimeUnit, ConcurrentHashMap, Future}
 
 import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.Producer
@@ -97,6 +96,20 @@ class KafkaSystemProducer(systemName: String,
     }
   }
 
+  def closeAndNullifyCurrentProducer(currentProducer: Producer[Array[Byte], Array[Byte]]) {
+    try {
+      currentProducer.close(0, TimeUnit.MILLISECONDS) // must use timeout to make sure we fail all waiting messages
+    } catch {
+      case e: Exception => error("producer close failed", e)
+    }
+    producerLock.synchronized {
+      if (currentProducer == producer) {
+        // only nullify the member producer if it is still the same object, no point nullifying new producer
+        producer = null
+      }
+    }
+  }
+
   def send(source: String, envelope: OutgoingMessageEnvelope) {
     trace("Enqueuing message: %s, %s." format (source, envelope))
 
@@ -113,6 +126,7 @@ class KafkaSystemProducer(systemName: String,
     val exception = sourceData.exceptionThrown
     if (exception != null) {
       metrics.sendFailed.inc
+      sourceData.exceptionThrown = null  // in case the caller catches all exceptions and will try again
       throw exception
     }
 
@@ -151,13 +165,18 @@ class KafkaSystemProducer(systemName: String,
                 metrics.sendSuccess.inc
               }
               else {
-                //If there is an exception in the callback, fail container!
-                //Close producer.
-                currentProducer.close
+                // If there is an exception in the callback - close the producer. If the caller doesn't catch this
+                // exception it will fail the container.
+                closeAndNullifyCurrentProducer(currentProducer)
+
+                error("closing the producer because of an exception in callback: " + exception.getMessage)
+                // we do not allow the reopenning of the produce here, because the exception happen
+                // in a separate (callback) thread. And user has no control now.
                 sourceData.exceptionThrown = new SamzaException("Unable to send message from %s to system %s." format(source, systemName),
-                                                     exception)
+                                                                 exception)
+
                 metrics.sendFailed.inc
-                logger.error("Unable to send message on Topic:%s Partition:%s" format(topicName, partitionKey),
+                error("Unable to send message on Topic:%s Partition:%s" format(topicName, partitionKey),
                              exception)
               }
             }
@@ -167,7 +186,9 @@ class KafkaSystemProducer(systemName: String,
       metrics.sends.inc
     } catch {
       case e: Exception => {
-        currentProducer.close()
+        closeAndNullifyCurrentProducer(currentProducer)
+
+        error("closing the producer because of an exception in send: ", e)
         metrics.sendFailed.inc
         throw new SamzaException(("Failed to send message on Topic:%s Partition:%s Exception:\n %s,")
           .format(topicName, partitionKey, e))
